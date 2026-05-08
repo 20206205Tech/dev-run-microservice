405947f27f339da99fe8d57dbb29cd8708a46efe
vuvannghiawork <vuvannghia.work@gmail.com>
2026-05-09 00:15:40 +0700

    3

--- scripts/commit.bat
+++ scripts/commit.bat
@@ -7,19 +7,24 @@ timeout /t 6 /nobreak
 
 cd /d "C:\Users\Admin\Documents\GitHub\code-chatbot-service"
 git nghia-git-commit-no-verify
+git nghia-git-commit-no-verify
 
 
 cd /d "C:\Users\Admin\Documents\GitHub\code-document-service"
+git nghia-git-commit-no-verify
 git nghia-git-commit-no-verify
 
 
 cd /d "C:\Users\Admin\Documents\GitHub\code-conversation-service"
 git nghia-git-commit-no-verify
+git nghia-git-commit-no-verify
 
 
 cd /d "C:\Users\Admin\Documents\GitHub\code-payment-service"
 git nghia-git-commit-no-verify
+git nghia-git-commit-no-verify
 
 
 cd /d "C:\Users\Admin\Documents\GitHub\code-fe-ui"
+git nghia-git-commit-no-verify
 git nghia-git-commit-no-verify
