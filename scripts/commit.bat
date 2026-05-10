0856312f3d29725b4b0abf94e2e913432833e856
vuvannghiawork <vuvannghia.work@gmail.com>
2026-05-10 19:29:23 +0700

    NO-VERIFY

--- scripts/commit.bat
+++ scripts/commit.bat
@@ -4,25 +4,20 @@ timeout /t 1 /nobreak
 :: Đặt đường dẫn thư mục gốc
 set "ROOT_DIR=C:\Users\Admin\Documents\GitHub"
 
-:: Lặp qua tất cả các thư mục con cấp 1 bên trong ROOT_DIR
+@REM echo Bat dau xu ly cac Git repository...
+
 for /D %%F in ("%ROOT_DIR%\*") do (
-    @REM echo.
-    @REM echo ========================================================
-    echo Đang xu ly folder: %%F
-    @REM echo ========================================================
-    
-    :: Di chuyển vào thư mục con
-    pushd "%%F"
-    
-    :: Thực thi lệnh git của bạn
-    git nghia-git-commit-no-verify
-    git nghia-git-commit-no-verify
-    
-    :: Trở về lại thư mục gốc để tiếp tục vòng lặp an toàn
-    popd
+    :: Chỉ thực thi nếu folder chứa thư mục .git
+    if exist "%%F\.git" (
+        echo Đang xu ly: %%~nxF
+        pushd "%%F"
+        
+        :: Chạy 1 lần (trừ khi bạn cố tình muốn chạy 2 lần)
+        git nghia-git-commit-no-verify
+        git nghia-git-commit-no-verify
+        
+        popd
+    )
 )
 
-@REM echo.
-@REM echo ========================================================
-@REM echo Da xu ly xong tat ca cac folder!
-@REM @REM pause
+@REM echo Hoan thanh!
