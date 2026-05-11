7a7d1a06aba31a44aac0decb6a087bf3e07cbd47
vuvannghiawork <vuvannghia.work@gmail.com>
2026-05-11 11:59:43 +0700

    NO-VERIFY

--- scripts/run.bat
+++ scripts/run.bat
@@ -66,7 +66,7 @@ timeout /t 10 /nobreak
 
 
 cd /d "C:\Users\Admin\Documents\GitHub\code-persona-service"
-start "Chatbot Service" "C:\Program Files\Git\bin\bash.exe" --login -i -c "doppler setup --project 20206205tech --config dev && doppler run -- uv run python main.py; exec bash"
+start "persona Service" "C:\Program Files\Git\bin\bash.exe" --login -i -c "doppler setup --project 20206205tech --config dev && doppler run -- uv run python main.py; exec bash"
 timeout /t 10 /nobreak
 
 
