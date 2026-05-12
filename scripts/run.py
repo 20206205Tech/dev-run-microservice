5f86324b8000197657acf7cb3305134f35eb0d74
vuvannghiawork <vuvannghia.work@gmail.com>
2026-05-12 12:32:55 +0700

    NO-VERIFY

--- scripts/run.py
+++ scripts/run.py
@@ -1,28 +1,7 @@
 
 
-# import subprocess
+import subprocess
 import time
-
-
-
-# # 1. Định nghĩa đường dẫn và lệnh
-# working_dir = r"C:\Users\Admin\Documents\GitHub\code-conversation-service"
-# bash_exe = r"C:\Program Files\Git\bin\bash.exe"
-# bash_cmd = "doppler setup --project 20206205tech --config dev && doppler run -- npm run start:dev; exec bash"
-
-
-
-# # print("Đang khởi động Conversation Service...")
-
-
-
-# # 2. Thay thế lệnh 'start' và 'cd /d'
-# # Dùng cwd để thay đổi thư mục làm việc (giống cd /d)
-# # Gọi cmd /c start để mở một cửa sổ console mới y hệt như file .bat
-# subprocess.Popen(
-#     ["cmd", "/c", "start", '"Conversation Service"', bash_exe, "--login", "-i", "-c", bash_cmd],
-#     cwd=working_dir
-# )
 
 
 
@@ -44,5 +23,27 @@ print (r"|__/     |__/|__/ \_______/|__/       \______/ |_______/  \_______/|__/
 
 time.sleep(5)
 
+
+
+
+
+# 1. Định nghĩa đường dẫn và lệnh
+working_dir = r"C:\Users\Admin\Documents\GitHub\code-conversation-service"
+bash_exe = r"C:\Program Files\Git\bin\bash.exe"
+bash_cmd = "doppler setup --project 20206205tech --config dev && doppler run -- npm run start:dev; exec bash"
+
+
+
+# print("Đang khởi động Conversation Service...")
+
+
+
+# 2. Thay thế lệnh 'start' và 'cd /d'
+# Dùng cwd để thay đổi thư mục làm việc (giống cd /d)
+# Gọi cmd /c start để mở một cửa sổ console mới y hệt như file .bat
+subprocess.Popen(
+    ["cmd", "/c", "start", '"Conversation Service"', bash_exe, "--login", "-i", "-c", bash_cmd],
+    cwd=working_dir
+)
 
 
