29828d7c0e0234e5425ce5691ac3c076404f6497
vuvannghiawork <vuvannghia.work@gmail.com>
2026-05-29 20:22:57 +0700

    NO-VERIFY - 2026-05-29 20:22:57

--- scripts/run.bat
+++ scripts/run.bat
@@ -11,32 +11,32 @@ echo "| $$\  $ | $$| $$| $$      | $$      | $$  | $$ \____  $$| $$_____/| $$   
 echo "| $$ \/  | $$| $$|  $$$$$$$| $$      |  $$$$$$/ /$$$$$$$/|  $$$$$$$| $$         \  $/   | $$|  $$$$$$$|  $$$$$$$"
 echo "|__/     |__/|__/ \_______/|__/       \______/ |_______/  \_______/|__/          \_/    |__/ \_______/ \_______/"
 
-
+---
 
 echo Mo: vuvannghia-note
 cd /d "C:\Users\Admin\Desktop\Nghia\gitlab\vuvannghia\note\vuvannghia-note\data"
-start "" code .
-timeout /t 2 /nobreak >nul
+start "" cmd /c "code ."
+timeout /t 50 /nobreak >nul
 
 echo Mo: code-payment-service
 cd /d "C:\Users\Admin\Documents\GitHub\code-payment-service" 
-start "" code .
-timeout /t 2 /nobreak >nul
+start "" cmd /c "code ."
+timeout /t 50 /nobreak >nul
 
 echo Mo: code-conversation-service
 cd /d "C:\Users\Admin\Documents\GitHub\code-conversation-service"
-start "" code .
-timeout /t 2 /nobreak >nul
+start "" cmd /c "code ."
+timeout /t 50 /nobreak >nul
 
 echo Mo: code-chatbot-service
 cd /d "C:\Users\Admin\Documents\GitHub\code-chatbot-service"
-start "" code .
-timeout /t 2 /nobreak >nul
+start "" cmd /c "code ."
+timeout /t 50 /nobreak >nul
 
 echo Mo: code-persona-service
 cd /d "C:\Users\Admin\Documents\GitHub\code-persona-service"
-start "" code .
-timeout /t 2 /nobreak >nul
+start "" cmd /c "code ."
+timeout /t 50 /nobreak >nul
 
 @REM cd /d "C:\Users\Admin\Documents\GitHub\code-document-service"
 @REM cd /d "C:\Users\Admin\Documents\GitHub\code-document-worker"
@@ -45,8 +45,9 @@ timeout /t 2 /nobreak >nul
 
 echo Mo: code-fe-ui va Browser
 cd /d "C:\Users\Admin\Documents\GitHub\code-fe-ui"
-start "" code .
-timeout /t 1 /nobreak >nul
+start "" cmd /c "code ."
+timeout /t 50 /nobreak >nul
 start http://localhost:3000
 
-echo Tat ca da duoc mo theo thu tu!
+:: Thoat va dong cua so CMD ngay lap tuc
+exit
